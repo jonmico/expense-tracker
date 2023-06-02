97785561862720424db0f51c4b6ca25700c7ac00
Jon Mico <jonmico@gmail.com>
2023-06-02 12:13:53 -0400

    Add handling for ExpenseForm.

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,56 @@
 import './ExpenseForm.css';
+import { useState } from 'react';
 
 export default function ExpenseForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    amount: '',
+    date: '',
+  });
+
+  function handleFormDataChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
-          <label htmlFor=''>Title</label>
-          <input type='text' />
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            value={formData.title}
+            name='title'
+            id='title'
+            onChange={handleFormDataChange}
+            placeholder='Expense Title'
+          />
         </div>
         <div className='new-expense__control'>
-          <label htmlFor=''>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <label htmlFor='amount'>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={formData.amount}
+            name='amount'
+            id='amount'
+            onChange={handleFormDataChange}
+            placeholder='0.00'
+          />
         </div>
         <div className='new-expense__control'>
           <label htmlFor=''>Date</label>
-          <input type='date' min='2019-01-01' max='2023-12-31' />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2023-12-31'
+            value={formData.date}
+            name='date'
+            id='date'
+            onChange={handleFormDataChange}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
